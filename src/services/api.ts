b9ed32c76686570fd48863fd2440373286e2abc0
Siddhu054 <siddharthaballa123@gmail.com>
2025-03-03 21:38:27 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = https://minilink1.onrender.com/ || 'http://localhost:8080';
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 // Create axios instance with base URL
 const api = axios.create({
